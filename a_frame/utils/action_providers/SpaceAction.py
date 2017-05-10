de8cddc87dd26a37d1fb0984d3299cd62072b4c1
Akshay Karanth <akshaypkaranth@gmail.com>
2017-05-10 00:56:06 -0400

    Inheriting SpaceAction from NetConfAction

--- a_frame/utils/action_providers/SpaceAction.py
+++ a_frame/utils/action_providers/SpaceAction.py
@@ -1,73 +1,146 @@
 import abc
 from a_frame.utils.action_providers.action_base import ActionBase
+from jnpr.junos import Device
+from jnpr.junos.utils.config import Config
+from jnpr.junos.exception import *
 from lxml import etree
-import urllib2
-import base64
-import platform
+import re
 import json
-import ssl
-from urllib2 import HTTPError
 
-import argparse
 
 class SpaceAction(ActionBase):
     """
-        Simple SPACE action provider
-        This is a "standalone" action, meaning it will be executed without an endpoint being passed in
+        Uses NetConf to execute the given template
+        Can execute template as an op command
+        or apply the template as a configuration template
     """
 
-    # inherited set_global_options from action_base.py will overwrite all of these automatically from the
-    # "create_template" selections
- 	username = ""
-	password = ""
-	salt_master_ip = ""
+    dev = None
+    request_type = "apply_template"
+    result_msg = ""
+
+    def set_endpoint(self, endpoint):
+        try:
+            # create the required iterator from the endpoint_list
+            self.dev = Device(user=endpoint["username"], password=endpoint["password"], host=endpoint["ip"])
+            self.dev.open(gather_facts=False)
+
+        except Exception as err:
+            print "Could not open device!"
+            self.result_msg = str(err)
+            self.dev = None
+
+        return
 
     def execute_template(self, template):
         """
-        We now have the global config (if any), the instance config, and the completed template from the input_form
-        and can perform the desired request
-        :param template: the completed template from the user or API
-        :return Boolean based on execution outcome.
+        switches based on request_type chosen from template configuration
+        :param template:
+        :return: String results from the endpoint netconf subsystem
         """
-        # print "executing %s" % template
+        if self.dev is None:
+            return self.result_msg
+
+        if self.request_type == "apply_template":
+            return self.apply_template(template)
 
 
 
-        # ensure no CRLF has snuck through
-        template = template.replace('\r\n', '\n')
+    def apply_template(self, template):
+        print self.dev
+        conf_string = template.strip()
 
-        data = template + "\n\n"
-        print "Request type: %s" % self.request_type
+        print conf_string
 
-        if self.request_type == "publish_event":
+        if re.search(r"^&lt;", conf_string):
+            print "Found a encoded string"
+            conf_string = self.unescape(conf_string)
+
+        print conf_string
+        # try to determine the format of our config_string
+        config_format = "set"
+        if re.search(r"^\s*<.*>$", conf_string, re.MULTILINE):
+            print "found xml style config"
+            config_format = "xml"
+        elif re.search(r"^\s*(set|delete|replace|rename)\s", conf_string):
+            print "found set style config"
+            config_format = "set"
+        elif re.search(r"^[a-z:]*\s*\w+\s+{", conf_string, re.I) and re.search(r".*}\s*$", conf_string):
+            print "found a text style config"
+            config_format = "text"
+
+        print "using format: " + config_format
+        cu = Config(self.dev)
+        try:
+            cu.lock()
+        except LockError as le:
+            print "Could not lock database!"
+            print str(le)
+            self.dev.close()
+            return "Failed to lock configuration database! %s" % str(le)
+
+        try:
+            print "loading config"
+            cu.load(conf_string, format=config_format)
+        except Exception as e:
+            print "Could not load configuration"
+            print str(e)
             try:
-                
-                json_in_string =  self.format_results(template)
-		print 'Parsing Args from JEAP'
-		parser = argparse.ArgumentParser(description='saltstack event system')
+                cu.unlock()
+            except UnlockError as ue:
+                print str(ue)
 
-		parser.add_argument('device_ip', type=str, help='IP of Juniper Device')
-		print 'firing event to salt master!'
+            self.dev.close()
+            return "Failed, could not load the configuration template. %s" % str(e)
 
-		caller=salt.client.Caller()
-		caller.sminion.functions['event.send'](
-		'myco/myevent/success',
-		{
-		'message': json_in_string
-		})
+        diff = cu.diff()
+        print diff
+        if diff is not None:
+            try:
+                cu.commit_check()
+                print "Committing config!"
+                cu.commit(comment="Commit via a_frame")
 
-		print 'DONE SENDING TO SALT MASTER'
+            except CommitError as ce:
+                print "Could not load config! %s" % str(ce)
+                cu.rollback()
+                try:
+                    print "Unlocking database!"
+                    cu.unlock()
+                except UnlockError as ue:
+                    print "Could not unlock database"
+                    print str(ue)
+                print repr(ce)
+                self.dev.close()
+                return "Failed, commit check failed. %s" % str(ce)
 
-
-            except Exception as ex:
-                print str(ex)
-                return "Error! %s" % str(ex)
         else:
-            # this is a subscribe_to_event attempt
-            try:
-                print('subscribe_to_event yet to be implemented')
-            except Exception as ex:
-		print('exception for subscribe')
+            # nothing to commit
+            print "Nothing to commit - no diff found"
+            cu.unlock()
+            self.dev.close()
+            return "Nothing to commit!"
+
+        try:
+            print "Unlocking database!"
+            cu.unlock()
+        except UnlockError as ue:
+            print "Could not unlock database"
+            print str(ue)
+            self.dev.close()
+            return "Committed, but could not unlock db"
+
+        print "Closing device handle"
+        self.dev.close()
+        return "Completed with diff: %s" % diff
+
+    @staticmethod
+    def unescape(s):
+        s = s.replace("&lt;", "<")
+        s = s.replace("&gt;", ">")
+        s = s.replace("&amp;", "&")
+        return s
+
     @staticmethod
     def format_results(results):
         """
@@ -75,45 +148,28 @@ class SpaceAction(ActionBase):
         :param results: string from urlopen
         :return: formatted string output
         """
+        print results
+        try:
+            if type(results) is str:
+                results = etree.fromstring(results)
+
+            print "found valid xml, formatting and returning"
+            # use pretty_print if we have an xml document returned
+            return etree.tostring(results, pretty_print=True)
+        except ValueError:
+            pass
+        except TypeError:
+            pass
+
         # is the result valid json?
         try:
             json_string = json.loads(results)
             print "Found JSON results"
             return json.dumps(json_string, indent=4)
         except ValueError:
-            # this isn't xml or json, so just return it!
-            print "Unknown results!"
-            return results
+            pass
+        except TypeError:
+            pass
 
-    def connect_to_keystone(self):
-        """
-        connects to Keystone in the specified project scope
-        :return: boolean if successful
-        """
-
-        _auth_json = """
-            { "auth": {
-                "identity": {
-                  "methods": ["password"],
-                  "password": {
-                    "user": {
-                      "name": "%s",
-                      "domain": { "id": "default" },
-                      "password": "%s"
-                    }
-                  }
-                },
-                  "scope": {
-                        "project": {
-                            "domain": {
-                                "id": "default"
-                            },
-                            "name": "%s"
-                        }
-                    }
-                }
-            }
-            """ % (self.username, self.password, self.keystone_project)
-
-      
-        return True
+        print "no special formatting, returning"
+        return results
